613e2880ca2e7bdc216201f88eeae5574f49e706
Maria Barrera <mbarrera_usa@yahoo.com>
2021-03-13 22:59:56 -0800

    Update scrape_mars.py

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -47,6 +47,51 @@ def scrape_info():
     # News Title and Paragraph Text.
     # Assign the text to variables that you can reference later.
 
+    # Get the latest News Title and Paragraph Text.
+    news_title = soup.find_all('div', class_ = 'content_title')[0].text
+    news_title
+
+    news_paragraph = soup.find_all('div', class_ = 'article_teaser_body')[0].text
+    news_paragraph
+
+    # ------------------------------------------
+    # - JPL Mars Space Images - Featured Image
+    # ------------------------------------------
+
+    # Visit the following URL
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    #soup
+
+    # Examine the results, then determine element that contains sought info
+    #print(soup.prettify())
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    #soup
+
+    # find all images
+    for item in soup.find_all('img'): 
+        print(item['src'])
+
+    # url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    result = url.find('index.html') 
+    print(result)
+    root = url[0:result]
+
+    # Retrieve featured image link
+    relative_image_path = soup.find_all('img')[1]["src"]
+    featured_image_url = root + relative_image_path
+    featured_image_url
+
+    relative_image_path
 
 
 
