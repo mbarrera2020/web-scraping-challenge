ebec8f6ce2d060bf894d994efc382db6434a359d
Maria Barrera <mbarrera_usa@yahoo.com>
2021-03-14 18:44:00 -0700

    web-scraping-modification
    20210314 – create index.html file

--- Missions_to_Mars/app_MissionToMars.py
+++ Missions_to_Mars/app_MissionToMars.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app2")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app2")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_info = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=mars_data)
+    return render_template("index.html", mars=mars_info)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_info = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
